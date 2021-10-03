dd2e123423e0423ed0bf73d3cd2f8130aea74d9a
Grzegorz Pasieka <pasieka.grzegorz@gmail.com>
2021-10-03 21:53:23 +0200

    Add localStorage for last succesfull city

--- src/App.tsx
+++ src/App.tsx
@@ -13,58 +13,69 @@ import './components/Input.css';
 export enum Unit {
   Standard = 'standard',
   Metric = 'metric',
-  Imperial = 'imperial'
+  Imperial = 'imperial',
 }
 
 export type Coord = {
-  lon: number,
-  lat: number
-}
+  lon: number;
+  lat: number;
+};
 
 export type Weather = {
-  id: number,
-  main: string,
-  description: string
-}
+  id: number;
+  main: string;
+  description: string;
+};
 
 export type ForecastType = {
-  name: string,
-  coord: Coord,
-  weather: Weather[],
+  name: string;
+  coord: Coord;
+  weather: Weather[];
   main: {
-    temp: number,
-    feels_like: number,
-    temp_min: number,
-    temp_max: number
-  }
-}
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+  };
+};
+
+const searchPhraseStorageKey = 'lastSearchPhrase';
 
 export const App: React.FC = () => {
   const [unit] = useState(Unit.Metric);
   const [currentForecast, setCurrentForecast] = useState<ForecastType | null>(null);
+  const lastSearchPhrase = localStorage.getItem(searchPhraseStorageKey);
 
   return (
     <Fragment>
       <Header>
         <Formik
-					initialValues={{search: ''}}
-					onSubmit={async (values, {resetForm}) => {
-            setCurrentForecast(await getCurrentWeather(values.search, unit));
-            resetForm();
+          initialValues={{ search: lastSearchPhrase || '' }}
+          onSubmit={async (values, { resetForm }) => {
+            try {
+              const result = await getCurrentWeather(values.search, unit);
+              if (result.cod !== 200) {
+                throw new Error(`Error while fetching weather. ${result.message}`);
+              }
+              setCurrentForecast(result);
+              resetForm();
+              localStorage.setItem(searchPhraseStorageKey, values.search);
+            } catch (error) {
+              // TODO: error handling
+              console.error(error);
+            }
           }}
-					>
-					<Form>
-            <div className="search-input">
-              <Field name="search" placeholder="Location..." />
-              <span className="icon" />
+        >
+          <Form>
+            <div className='search-input'>
+              <Field name='search' placeholder='Location...' />
+              <span className='icon' />
             </div>
-					</Form>
-				</Formik>
+          </Form>
+        </Formik>
       </Header>
-      <Body>
-        { currentForecast && <Forecast forecast={currentForecast} units={unit} /> }
-      </Body>
+      <Body>{currentForecast && <Forecast forecast={currentForecast} units={unit} />}</Body>
       <Footer />
     </Fragment>
   );
-}
+};
